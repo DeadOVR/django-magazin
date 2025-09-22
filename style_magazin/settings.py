7155e4febe2817ec4cc8e20ff684a260e2933b29
CatPython1234 <grisanikon@gmail.com>
2025-09-22 04:33:57 +0300

    Добавил приложение users для регистрации.

--- style_magazin/settings.py
+++ style_magazin/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
     'main.apps.MainConfig',
     'cart.apps.CartConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -144,3 +145,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 SESSION_COOKIE_AGE = 86400 # 30 дней
 SESSION_SAVE_EVERY_REQUEST = True
+
+
+AUTH_USER_MODEL = 'users.CustomUser'
