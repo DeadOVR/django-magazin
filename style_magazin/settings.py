23a459a3ccbe6b68eeeb430098be7fd0900ea9c4
CatPython1234 <grisanikon@gmail.com>
2025-09-23 14:16:22 +0300

    Начинаю деплоить.

--- style_magazin/settings.py
+++ style_magazin/settings.py
@@ -27,10 +27,22 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'coolsait.ru', 'www.coolsait.ru']
 
+CSRF_TRUSTED_ORIGINS = [
+    'http://127.0.0.1:8000'
+    'http://localhost:8000'
+    'http://coolsait.ru'
+    'http://www.coolsait.ru'
+]
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+X_FRAME_OPTIONS = 'DENY'
 
 # Application definition
 
